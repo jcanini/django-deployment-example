167447f9fc9d3e9432fb4fbdc9e8eb05343a73fd
jesse <jesse@pinot.fritz.box>
2018-10-01 22:20:09 +1300

    first commit

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -27,7 +27,6 @@ SECRET_KEY = 'qqcszn3$29g=92*b0)kct*k$&_f*-$ckd!%*oy_o++*q@itx68'
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1',
-                'mirost.ddns.net',
                 ]
 
 
@@ -41,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'basic_app',
-    'mod_wsgi.server',
 ]
 
 MIDDLEWARE = [
